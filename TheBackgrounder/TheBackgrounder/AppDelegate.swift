89db75ed03763e342a462b0ab885d71d095d7717
wei.mao <wei.mao@jimubox.com>
2016-10-27 18:43:30 +0800

    Background Fetch

--- TheBackgrounder/TheBackgrounder/AppDelegate.swift
+++ TheBackgrounder/TheBackgrounder/AppDelegate.swift
@@ -30,9 +30,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
-    
+    UIApplication.shared.setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalMinimum)
+
     
     return true
   }
   
+  // Support for background fetch
+  func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    if let tabBarController = window?.rootViewController as? UITabBarController,
+      let viewControllers = tabBarController.viewControllers
+    {
+      for viewController in viewControllers {
+        if let fetchViewController = viewController as? FetchViewController {
+          fetchViewController.fetch {
+            fetchViewController.updateUI()
+            completionHandler(.newData)
+          }
+        }
+      }
+    }
+  }
 }
